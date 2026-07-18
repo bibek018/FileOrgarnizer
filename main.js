7782aa6ef8b3d6a700e08cb50ca7a494e11bbd21
Bibek Ojha <ojhabibek505@gmail.com>
2026-07-18 11:21:13 +0545

    Optimized the function

--- main.js
+++ main.js
@@ -5,14 +5,28 @@ import { pathVerify } from "./pathVerify.js";
 import { fetchDir } from "./fetchDir.js";
 import { extFetch } from "./extFetch.js";
 import { fileStats } from "./pathVerify.js";
-import {  isUnique,  generateUniqueName } from "./handleDuplicacy.js";
+import { isUnique, generateUniqueName } from "./handleDuplicacy.js";
+const EXT_MAP = {
+  ".png": "Images",
+  ".jpg": "Images",
+  ".jpeg": "Images",
+  ".doc": "Documents",
+  ".txt": "Documents",
+  ".csv": "Documents",
+  ".pptx": "Documents",
+  ".xlsx": "Documents",
+  ".pdf": "Documents",
+  ".mp4": "Media",
+  ".mp3": "Media",
+  ".mkv": "Media",
+};
 const Main = async (dirpath) => {
   try {
     await pathVerify(dirpath);
     const files = await fetchDir(dirpath);
     let fileCounter = 0;
-
     const foldersUsed = new Set();
+
     for (const file of files) {
       const fullpath = path.join(dirpath, file);
       const stats = await fileStats(fullpath);
@@ -21,60 +35,17 @@ const Main = async (dirpath) => {
         continue;
       }
       const ext = await extFetch(file);
+      const subdir = EXT_MAP[ext] || "Others";
       const source = path.join(dirpath, file);
-
       let destination = "";
-      let subdir = "";
-      
-      if (ext == ".png" || ext == ".jpg" || ext == ".jpeg") {
-        subdir = "Images";
-        const dir_subdir = path.join(dirpath, subdir);
-        if (await isUnique(dir_subdir, file)) {
-          destination = path.join(dirpath, subdir, file);
-        } else {
-          const newname = await generateUniqueName(dir_subdir, file);
-          destination = path.join(dirpath, subdir, newname);
-        }
-        foldersUsed.add(subdir);
-      } else if (
-        ext == ".doc" ||
-        ext == ".txt" ||
-        ext == ".csv" ||
-        ext == ".pptx" ||
-        ext == ".xlsx" ||
-        ext == ".pdf"
-      ) {
-        subdir = "Documents";
-        const dir_subdir = path.join(dirpath, subdir);
-        if (await isUnique(dir_subdir, file)) {
-          destination = path.join(dirpath, subdir, file);
-        } else {
-          const newname = await generateUniqueName(dir_subdir, file);
-          destination = path.join(dirpath, subdir, newname);
-        }
-        foldersUsed.add(subdir);
-      } else if (ext == ".mp4" || ext == ".mp3" || ext == ".mkv") {
-        subdir = "Media";
-        const dir_subdir = path.join(dirpath, subdir);
-        if (await isUnique(dir_subdir, file)) {
-          destination = path.join(dirpath, subdir, file);
-        } else {
-          const newname = await generateUniqueName(dir_subdir, file);
-          destination = path.join(dirpath, subdir, newname);
-        }
-        foldersUsed.add(subdir);
-      } else {
-        subdir = "Others";
+      const dir_subdir = path.join(dirpath, subdir);
+      if (await isUnique(dir_subdir, file)) {
         destination = path.join(dirpath, subdir, file);
-        const dir_subdir = path.join(dirpath, subdir);
-        if (await isUnique(dir_subdir, file)) {
-          destination = path.join(dirpath, subdir, file);
-        } else {
-          const newname = await generateUniqueName(dir_subdir, file);
-          destination = path.join(dirpath, subdir, newname);
-        }
-        foldersUsed.add(subdir);
+      } else {
+        const newname = await generateUniqueName(dir_subdir, file);
+        destination = path.join(dirpath, subdir, newname);
       }
+      foldersUsed.add(subdir);
       await fs.mkdir(path.join(dirpath, subdir), { recursive: true });
       await fs.rename(source, destination);
       fileCounter++;
@@ -88,10 +59,9 @@ const Main = async (dirpath) => {
   }
 };
 
-try {
-  const dirpath = process.argv[2];
-  Main(dirpath);
-} catch (err) {
-  console.log(err.message);
-  console.log(err.code);
+const dirpath = process.argv[2];
+if (!dirpath) {
+  console.log("Please provide the directory path!");
+  process.exit(1);
 }
+Main(dirpath);
